281506d7dac7949ed5e491d04e54dd264d61c240
Valentin B <valentin.be@protonmail.com>
2023-03-29 00:08:08 +0200

    Add more miscellaneous macros and unified macros header

---
+++ include/vtils/macros.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file macros.hpp
+ * @brief Header for including all public-facing library macros.
+ * @copyright Valentin B.
+ */
+#pragma once
+
+#include "vtils/macros/attr.hpp"
+#include "vtils/macros/compiler.hpp"
+#include "vtils//macros/misc.hpp"
---
+++ include/vtils/macros/misc.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file misc.hpp
+ * @brief Miscellaneous useful macros.
+ * @copyright Valentin B.
+ */
+#pragma once
+
+#include <limits.h>
+
+#include "vtils/macros/attr.hpp"
+
+#define V_STRINGIFY_IMPL(s) #s
+/// Converts an arbitrary expression to a string literal.
+#define V_STRINGIFY(s) V_STRINGIFY_IMPL(s)
+
+#define V_CONCAT_IMPL(s1, s2) s1##s2
+/// Textually concatenates two given tokens.
+#define V_CONCAT(s1, s2) V_CONCAT_IMPL(s1, s2)
+
+/// Creates an anonymous variable with a unique name.
+#ifdef __COUNTER__
+    #define V_ANON_VAR(x) V_CONCAT(x, __COUNTER__)
+#else
+    #define V_ANON_VAR(x) V_CONCAT(x, __LINE__)
+#endif
+
+/// Gets the size of a type in bits.
+#ifndef BITSIZEOF
+    #define BITSIZEOF(T) (sizeof(T) * CHAR_BIT)
+#endif
+
+/// Discards unused variables without side effects.
+#define V_UNUSED(...) ::vtils::impl::UnusedImpl(__VA_ARGS__)
+
+namespace vtils::impl {
+
+    template <typename... Args>
+    ALWAYS_INLINE constexpr void UnusedImpl(Args &&...args) {
+        (static_cast<void>(args), ...);
+    }
+
+}
